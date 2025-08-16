17d2441df79ee3d82a302855135a539455519342
Eric Brown <eric@ebrown.net>
2025-08-16 10:45:16 -0600

    Pedal interceptor longitudinal control

--- opendbc/safety/safety_declarations.h
+++ opendbc/safety/safety_declarations.h
@@ -240,6 +240,7 @@ bool longitudinal_speed_checks(int desired_speed, const LongitudinalLimits limit
 bool longitudinal_gas_checks(int desired_gas, const LongitudinalLimits limits);
 bool longitudinal_transmission_rpm_checks(int desired_transmission_rpm, const LongitudinalLimits limits);
 bool longitudinal_brake_checks(int desired_brake, const LongitudinalLimits limits);
+bool longitudinal_interceptor_checks(const CANPacket_t *to_send);
 void pcm_cruise_check(bool cruise_engaged);
 void speed_mismatch_check(const float speed_2);
 
@@ -248,6 +249,7 @@ void safety_tick(const safety_config *safety_config);
 // This can be set by the safety hooks
 extern bool controls_allowed;
 extern bool relay_malfunction;
+extern bool enable_gas_interceptor;
 extern bool gas_pressed;
 extern bool gas_pressed_prev;
 extern bool brake_pressed;
