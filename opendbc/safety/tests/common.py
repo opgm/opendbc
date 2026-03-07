5151f72dd8bd8137300c7eb664f17fb62adb226e
Eric Brown <eric@ebrown.net>
2026-03-07 10:57:20 -0700

    CC longitudinal control

--- opendbc/safety/tests/common.py
+++ opendbc/safety/tests/common.py
@@ -971,7 +971,7 @@ class SafetyTest(SafetyTestBase):
               continue
             if {attr, current_test}.issubset({'TestGmCameraSafety', 'TestGmCameraLongitudinalSafety', 'TestGmAscmSafety',
                                               'TestGmCameraEVSafety', 'TestGmCameraLongitudinalEVSafety', 'TestGmAscmEVSafety',
-                                              'TestGmInterceptorSafety'}):
+                                              'TestGmInterceptorSafety', 'TestGmCcLongitudinalSafety'}):
               continue
             if attr.startswith('TestFord') and current_test.startswith('TestFord'):
               continue
